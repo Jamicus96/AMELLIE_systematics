9d2b9c290c6e3072fc5c40f6729487904bbd1f52
Jamicus96 <jepage999@gmail.com>
2022-05-02 11:36:19 -0400

    bug fix

--- scripts/GetFOM.cpp
+++ scripts/GetFOM.cpp
@@ -63,8 +63,8 @@ int main(int argc, char** argv){
         std::string file_address = tracking_hist_repo + "tot_AMELLIE_" + geo_name + "_" + str_lst.at(1) + "_" + str_lst.at(2) + "_reemis" + str_lst.at(3) + "_abs" + str_lst.at(4) + ".root";
         traking_files.push_back(file_address);
         std::cout << file_address << std::endl;
-        abs_scalings.push_back(std::stod(str_lst.at(3)));
-        std::cout << std::stod(str_lst.at(3)) << std::endl;
+        abs_scalings.push_back(std::stod(str_lst.at(4)));
+        std::cout << std::stod(str_lst.at(4)) << std::endl;
         if (abs_scalings.at(i) == 1.0) {
             abs1_idx = i;
         }
